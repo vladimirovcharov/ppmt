8ffb2115700fd05587d575a90da9bf7bd60f80b7
Vladimir Ovcharov <vladimir.1527@gmail.com>
2018-12-31 01:21:13 +0200

    Add react front-end

---
+++ ppmt/src/main/resources/static/precache-manifest.2affcb9b5ddf270a8473ed5e6248cb85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43759bcc13b3a064d438",
+    "url": "/static/css/main.f439de4f.chunk.css"
+  },
+  {
+    "revision": "43759bcc13b3a064d438",
+    "url": "/static/js/main.43759bcc.chunk.js"
+  },
+  {
+    "revision": "6cf95472e26e9ecb7f81",
+    "url": "/static/css/1.782fbc69.chunk.css"
+  },
+  {
+    "revision": "6cf95472e26e9ecb7f81",
+    "url": "/static/js/1.6cf95472.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7bc6ea1ea237d7ebbf88c1b2df2a1aaa",
+    "url": "/index.html"
+  }
+];
